339e143b7bf018591e27ef39965eb5958bac1dbb
Mark Brownsword <markbrownsword@gmail.com>
2018-04-21 19:59:49 +1000

    setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'helloNLTK',
+    version = '0.1.0',
+    packages = ['helloNLTK'],
+    entry_points = {
+        'console_scripts': [
+            'helloNLTK = helloNLTK.__main__:main'
+        ]
+    })
